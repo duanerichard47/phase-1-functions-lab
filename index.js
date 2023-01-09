bd76f33d277da3fe2e11a7b02387289fc5053827
Duane <duanerichard47@hotmail.com>
2023-01-09 17:17:51 -0500

    Completed phase 1 functions lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+   return Math.abs(42-someValue);
+   }
+    
+function distanceFromHqInFeet(someValue)  { 
+    return 264 * distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(start, destination){ 
+    return (Math.abs(start-destination) * 264);
+ }
+   
+/*
+function calculatesFarePrice(start, destination) { 
+    let totalFeet = distanceTravelledInFeet(start, destination)
+        if( totalFeet < 400) {
+            return 0;
+        }
+            else if (totalFeet >= 400 && totalFeet<= 2000) {
+                return  (totalFeet-400) *.02 ;
+            }
+                else if (totalFeet > 2000 && totalFeet < 2500) {
+                    return 25;
+                }
+                    else {
+                        return 'cannot travel that far';
+                    }
+  }
+    */
+  function calculatesFarePrice(start, destination) { 
+    let totalFeet = distanceTravelledInFeet(start, destination)
+   
+        switch (true){
+           case totalFeet < 400: return 0;
+           break;
+        
+            case totalFeet >= 400 && totalFeet<= 2000 : return  (totalFeet-400) *.02 ;
+            break;
+            
+            case totalFeet > 2000 && totalFeet< 2500 :return 25;
+            break;
+
+            default : return 'cannot travel that far';
+        }
+    }
